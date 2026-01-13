bb57d97d237e97fe0016599f6f44f308b62eab67
Geraldine743 <egeraldine2@gmail.com>
2026-01-13 18:56:33 +0100

    add API route test for backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/bonjour', function () {
+    return ['message' => 'Bravo ! Votre API fonctionne a merveille.'];
+});
