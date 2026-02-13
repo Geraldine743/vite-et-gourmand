afdfcc86c1f2e4fa7193c63e26b3e616a01645ee
Geraldine743 <egeraldine2@gmail.com>
2026-02-13 10:36:24 +0100

    add structure and style section presentation and update style footer

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,8 +2,34 @@
 </script>
 
 <template>
-  <main>
-    <h1>Bienvenue chez Vite et Gourmand 🍔</h1>
-    <p>Le site est prêt à être construit (avec TypeScript) !</p>
-  </main>
+<section class="presentation">
+  
+  <div class="presentation__media">
+    <img 
+      src="@/assets/images/traiteur1.jpg" 
+      alt="Traiteur Vite et Gourmand créations culinaires" 
+      class="presentation__img" 
+    />
+  </div>
+
+  <div class="presentation__content">
+    
+    <h2 class="presentation__title">
+      Créateur d'instants gourmands depuis 1999.
+    </h2>
+    
+    <p class="presentation__text">
+      À Bordeaux, la convivialité est une tradition. Chez Vite & Gourmand, c'est notre métier depuis 25 ans. 
+      Nous croyons que les meilleurs souvenirs se construisent autour d'une bonne table. 
+      C'est pourquoi nous sommes à vos côtés pour transformer vos événements en véritables fêtes des sens.
+    </p>
+    
+    <p class="presentation__text">
+      Du cocktail d'entreprise élégant au mariage féérique, en passant par le repas de famille dominical, 
+      nous mettons notre expérience au service de votre histoire. Laissez-nous vous guider, vous conseiller 
+      et dessiner avec vous un menu qui vous ressemble.
+    </p>
+
+  </div>
+</section>
 </template>
