baaa7d268453f333bc845ace0ee4fd9265873966
Geraldine743 <egeraldine2@gmail.com>
2026-03-25 13:04:55 +0100

    add template HTML MenuClientView.vue

--- frontend/src/views/MenuClientView.vue
+++ frontend/src/views/MenuClientView.vue
@@ -19,3 +19,49 @@ const fetchMenus = async () => {
 };
 onMounted(() => fetchMenus());
 </script>
+
+<template>
+    <div class="client-menus">
+        <h1 class="page-title text-center mb-5">Découvrez nos Menus</h1>
+        <div class="menu-grid">
+            <div v-for="menu in menus" :key="menu.id" class="menu-card">
+                
+                <div class="menu-card__carousel">
+                    <div class="badges-container">
+                        <span v-if="menu.theme" class="badge badge--theme">{{ menu.theme.libelle }}</span>
+                        <span v-if="menu.regime" class="badge badge--regime">{{ menu.regime.libelle }}</span>
+                    </div>
+
+                </div>
+
+                <div class="menu-card__content">
+                    <h2 class="menu-title">{{ menu.titre }}</h2>
+                    <p class="menu-description">{{ menu.description }}</p>
+
+                    <div class="menu-composition mt-3">
+                        <h3 class="section-subtitle">Composition :</h3>
+                        <ul>
+                            <li v-for="plat in menu.plats" :key="plat.id">{{ plat.titre_plat }}</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="menu-card__footer">
+                    <div class="infos-pratiques">
+                        <span class="price">{{ menu.prix_par_personne }} € <small>/ pers.</small></span>
+                        <span class="min-pers">👥 Min. {{ menu.nb_personne_min }} pers.</span>
+                    </div>
+                    
+                    <button 
+                        class="button btn-commander"
+                        :disabled="menu.stock <= 0"
+                        :class="{'out-of-stock': menu.stock <= 0}"
+                    >
+                        {{ menu.stock > 0 ? 'Commander' : 'Rupture de stock' }}
+                    </button>
+                </div>
+            </div>
+            
+        </div>        
+    </div>
+</template>
